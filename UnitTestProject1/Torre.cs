e50ec818579d2ebf62af72f8ada919ec8630c508
JuanJoseEspitia <juanjoseespitiag@gmail.com>
2022-03-13 16:36:25 -0500

    Avance -1 Espitia

--- UnitTestProject1/Torre.cs
+++ UnitTestProject1/Torre.cs
@@ -4,5 +4,16 @@ public class Torre
 {
 	public Torre()
 	{
+		Random rd = new Random();
+ 
+        int pisos = rd.Next(0,11);
+
+		List<int> TorreJugador = new List<int>(pisos);
+
+		for (int i = 0; i < pisos; i ++){
+
+			jugador.Jugador = TorreJugador[i];
+		}
+
 	}
 }
