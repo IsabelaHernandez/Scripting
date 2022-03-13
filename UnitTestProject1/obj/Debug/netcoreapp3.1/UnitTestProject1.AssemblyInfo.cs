9a0c07882529558587c989f888a9cec9d49ac004
Isa <isabelahdez03@gmail.com>
2022-03-13 13:33:44 -0500

    Script

--- UnitTestProject1/obj/Debug/netcoreapp3.1/UnitTestProject1.AssemblyInfo.cs
+++ UnitTestProject1/obj/Debug/netcoreapp3.1/UnitTestProject1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Pruebas_unitarias")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("UnitTestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Pruebas_unitarias")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Pruebas_unitarias")]
+[assembly: System.Reflection.AssemblyProductAttribute("UnitTestProject1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("UnitTestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
